38b672787900228c59e6b24036da33dab56b3b4f
Derek Weitzel <djw8605@gmail.com>
2020-04-10 12:18:16 -0500

    Use condor_submit wrapper on command line to capture scitokens and wrapper things

--- periodic-submit.py
+++ periodic-submit.py
@@ -2,6 +2,7 @@
 import htcondor
 import getpass
 import os
+import subprocess
 
 
 
@@ -17,16 +18,10 @@ def main():
     # We should always have from just below 1000 and 2000 idle jobs
     if idle_jobs < 1000:
         # Read in the submit file
+        # Have to use the condor_submit interface because of scitokens and wrapper stuff
         submit_file = ""
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
-        with open('covid.submit', 'r') as covid_submit:
-            submit_file = covid_submit.read()
-        
-        # Submit the number
-        sub = htcondor.Submit(submit_file)
-        sub.setQArgs("1000")
-        with schedd.transaction() as txn:
-            sub.queue(txn)
+        subprocess.call("condor_submit covid.submit", shell=True)
         print("Submitted 1000 jobs")
     else:
         print("More than 1000 idle jobs: {}".format(idle_jobs))
