1d7ffd12e084de84c053ef4775b70d324d1f7d62
Derek Weitzel <djw8605@gmail.com>
2020-04-10 12:36:17 -0500

    Hardcode the condor_submit wrapper

--- periodic-submit.py
+++ periodic-submit.py
@@ -21,7 +21,7 @@ def main():
         # Have to use the condor_submit interface because of scitokens and wrapper stuff
         submit_file = ""
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
-        subprocess.call("condor_submit covid.submit", shell=True)
+        subprocess.call("/usr/local/bin/condor_submit covid.submit", shell=True)
         print("Submitted 1000 jobs")
     else:
         print("More than 1000 idle jobs: {}".format(idle_jobs))
