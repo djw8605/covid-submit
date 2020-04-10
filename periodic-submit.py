7f151ea6660cfac47a33ddfc7ec19c5654d9eb85
Derek Weitzel <djw8605@gmail.com>
2020-04-10 15:58:29 -0500

    up to 4000

--- periodic-submit.py
+++ periodic-submit.py
@@ -14,9 +14,9 @@ def main():
     query_result = schedd.query(constraint = 'Owner =?= "{}" && JobStatus == 1'.format(username), attr_list=["JobStatus"])
     idle_jobs = len(query_result)
 
-    # If less than 1000 idle jobs, submit another 1000.  
-    # We should always have from just below 1000 and 2000 idle jobs
-    if idle_jobs < 2000:
+    # If less than 4000 idle jobs, submit another 1000.
+    # We should always have from just below 4000 and 5000 idle jobs
+    if idle_jobs < 4000:
         # Read in the submit file
         # Have to use the condor_submit interface because of scitokens and wrapper stuff
         submit_file = ""
