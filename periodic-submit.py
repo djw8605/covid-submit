76b3415f9035a16f5674327104e2914c4c73f475
Derek Weitzel <djw8605@gmail.com>
2020-04-15 13:12:51 -0500

    Adding eight core submissions

--- periodic-submit.py
+++ periodic-submit.py
@@ -11,8 +11,8 @@ def main():
     # Get my username for the query
     username = getpass.getuser()
     schedd = htcondor.Schedd()
-    query_result = schedd.query(constraint = 'Owner =?= "{}" && JobStatus == 1'.format(username), attr_list=["JobStatus"])
-    idle_jobs = len(query_result)
+    single_query_result = schedd.query(constraint = 'Owner =?= "{}" && JobStatus == 1 && RequestCPUs == 1'.format(username), attr_list=["JobStatus"])
+    idle_jobs = len(single_query_result)
 
     # If less than 8000 idle jobs, submit another 1000.
     # We should always have from just below 8000 and 9000 idle jobs
@@ -22,9 +22,27 @@ def main():
         submit_file = ""
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
         subprocess.call("/usr/local/bin/condor_submit covid.submit", shell=True)
-        print("Submitted 1000 jobs")
+        print("Submitted 1000 single core jobs")
     else:
-        print("More than 1000 idle jobs: {}".format(idle_jobs))
+        print("More than 1000 idle single core jobs: {}".format(idle_jobs))
+
+    eight_query_result = schedd.query(constraint = 'Owner =?= "{}" && JobStatus == 1 && RequestCPUs == 8'.format(username), attr_list=["JobStatus"])
+    idle_jobs = len(eight_query_result)
+
+    # If less than 2000 idle jobs, submit another 1000.
+    # We should always have from just below 2000 and 3000 idle jobs
+    if idle_jobs < 2000:
+        # Read in the submit file
+        # Have to use the condor_submit interface because of scitokens and wrapper stuff
+        submit_file = ""
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        subprocess.call("/usr/local/bin/condor_submit covid.submit -append request_cpus=8 -append arguments=8", shell=True)
+        print("Submitted 1000 eight core jobs")
+    else:
+        print("More than 1000 idle eight core jobs: {}".format(idle_jobs))
+    
+
+    
 
 if __name__ == "__main__":
     main()
--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 
 #singularity run docker://lukasheinrich/folding:latest FAHClient --user=CERN --team=38188 --gpu=false --smp=true
 cores=1
+smp=false
 if [ $# -ne 0 ]; then
 cores=$1
+smp=true
 fi
 
-FAHClient --user=Anonymous --team=258507 --gpu=false --smp=false --cpus=$cores --exit-when-done=true --max-units=1
+FAHClient --user=Anonymous --team=258507 --gpu=false --smp=$smp --cpus=$cores --exit-when-done=true --max-units=1
