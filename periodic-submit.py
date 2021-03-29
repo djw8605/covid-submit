516348af60c94a08c2c3c8a03ccffe41138f5e0f
Derek Weitzel <djw8605@gmail.com>
2021-03-29 15:16:32 -0500

    Stop submitting 1 core F@H jobs

--- periodic-submit.py
+++ periodic-submit.py
@@ -14,24 +14,24 @@ def main():
     # Get my username for the query
     username = getpass.getuser()
     schedd = htcondor.Schedd()
-    single_query_result = schedd.query(constraint = 'Owner =?= "{}" && RequestCPUs == 1'.format(username), attr_list=["JobStatus"])
-    total_jobs = len(single_query_result)
-    idle_jobs = 0
-    for job in single_query_result:
-        if job['JobStatus'] == 1:
-            idle_jobs += 1
+    #single_query_result = schedd.query(constraint = 'Owner =?= "{}" && RequestCPUs == 1'.format(username), attr_list=["JobStatus"])
+    #total_jobs = len(single_query_result)
+    #idle_jobs = 0
+    #for job in single_query_result:
+    #    if job['JobStatus'] == 1:
+    #        idle_jobs += 1
 
     # If less than 8000 idle jobs, submit another 1000.
     # We should always have from just below 8000 and 9000 idle jobs
-    if idle_jobs < max_idle_single_core and total_jobs < max_total_jobs:
-        # Read in the submit file
-        # Have to use the condor_submit interface because of scitokens and wrapper stuff
-        submit_file = ""
-        os.chdir(os.path.dirname(os.path.realpath(__file__)))
-        subprocess.call("/usr/local/bin/condor_submit covid.submit", shell=True)
-        print("Submitted 1000 single core jobs")
-    else:
-        print("Decided to not submit more jobs: idle_jobs:{}, total_jobs:{}".format(idle_jobs, total_jobs))
+    #if idle_jobs < max_idle_single_core and total_jobs < max_total_jobs:
+    #    # Read in the submit file
+    #    # Have to use the condor_submit interface because of scitokens and wrapper stuff
+    #    submit_file = ""
+    #    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    #    subprocess.call("/usr/local/bin/condor_submit covid.submit", shell=True)
+    #    print("Submitted 1000 single core jobs")
+    #else:
+    #    print("Decided to not submit more jobs: idle_jobs:{}, total_jobs:{}".format(idle_jobs, total_jobs))
 
     eight_query_result = schedd.query(constraint = 'Owner =?= "{}" && RequestCPUs == 8'.format(username), attr_list=["JobStatus"])
     total_jobs = len(eight_query_result)
